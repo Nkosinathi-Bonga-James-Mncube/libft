030a9d729e2ea1bb758a763c77de41f2f36bdb15
Nkosinathi MNCUBE <nmncube@c5r3s6.wethinkcode.co.za>
2019-05-20 12:49:34 +0200

    Added ft_atoi.c

--- ft_atoi/ft_atoi.c
+++ ft_atoi/ft_atoi.c
@@ -6,37 +6,29 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:48:35 by nmncube           #+#    #+#             */
-/*   Updated: 2019/05/20 11:51:33 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/05/20 12:48:48 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
+
 int		ft_atoi(const char *str)
-{		
-		int bfound;
-		int value;
-		int k;
-		value = 0;
-		bfound = 1;
-		k = 0;
-		
-		if (((*str >= 8) && (*str <= 13)) || (*str == 32))
-			str ++;
-		if (str[0] == '-')
-			bfound = -1;
-		while ((str[k]>= 48) && (str[k] <= 57) && (str[k] != '\0'))
-		{	value = (value * 10) +str[k] - 48;
-			k++;
-		}
-		value  = value *bfound;
-		return (value);
-}
-int		main(int argc, char **argv)
-{		
-		if (argc == 2)
-		printf("System %d\n", atoi(argv[1]));
-		printf("ft_atoi: %d\n" , ft_atoi(argv[1]));
-		return (0);
+{
+	int bfound;
+	int value;
+	int k;
+
+	value = 0;
+	bfound = 1;
+	k = 0;
+	while ((*str >= 8 && *str <= 13) || (*str == 32))
+		str++;
+	if (str[0] == '-')
+		bfound = -1;
+	while ((str[k] >= 48 && str[k] <= 57) && (str[k] != '\0'))
+	{
+		value = (value * 10) + str[k] - 48;
+		k++;
+	}
+	return (value);
 }
