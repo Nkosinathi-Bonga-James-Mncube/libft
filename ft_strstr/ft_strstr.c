b6a6be9c9faac0f72dfe6a56a89d1ae6cb5af1bb
Nkosinathi MNCUBE <nmncube@c5r3s6.wethinkcode.co.za>
2019-05-20 15:17:09 +0200

    New ft_strstr

---
+++ ft_strstr/ft_strstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/20 14:18:14 by nmncube           #+#    #+#             */
+/*   Updated: 2019/05/20 15:15:37 by nmncube          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+char *ft_strstr( const char *s1, const char *s2)
+{
+	int bfound;
+
+	bfound = 0;
+	while (*s1 != *s2)
+		s1 ++;
+	while (*s1 != '\0')
+	{
+	if (*s1 == *s2)
+		bfound = bfound + 0;
+	else
+		bfound = bfound + 1;
+		s1++;
+	}
+	if (bfound == 0)
+		return (s2);
+}
+int	main()
+{
+	printf("ft_strstr: %s\n",ft_strstr("bedroom" , "room"));
+	printf("%s\n" , strstr("bedroom","room"));
+	return (0);
+}
