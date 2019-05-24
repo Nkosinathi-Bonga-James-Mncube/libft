67ede30cdced3eeaad8983f5a0adf81fae7f415b
Nkosinathi MNCUBE <nmncube@c5r3s6.wethinkcode.co.za>
2019-05-24 10:24:17 +0200

    Added ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,24 +6,24 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 09:41:09 by nmncube           #+#    #+#             */
-/*   Updated: 2019/05/24 10:16:18 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/05/24 10:23:47 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int total;
 
 	total = 0;
-	while(n > 0)
+	while (n > 0)
 	{
-		if(*s1 != *s2)
+		if (*s1 != *s2)
 		{
 			total = *s1 - *s2;
 			break ;
-		}	
+		}
 		s1++;
 		s2++;
 		n--;
