2fed43abba821799fa2e55cc9dc5157130257fef
Nkosinathi MNCUBE <nmncube@c4r5s8.wethinkcode.co.za>
2019-05-30 16:00:50 +0200

    Added ft_strstr.c

--- ft_strstr/ft_strstr.c
+++ ft_strstr/ft_strstr.c
@@ -6,14 +6,14 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 14:18:14 by nmncube           #+#    #+#             */
-/*   Updated: 2019/05/30 15:34:57 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/05/30 16:00:15 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
 
-char *ft_strstr(const char *haystack, const char *needle)
+char	*ft_strstr(const char *haystack, const char *needle)
 {
 	int bfound;
 	int k;
@@ -21,29 +21,21 @@ char *ft_strstr(const char *haystack, const char *needle)
 	bfound = 0;
 	k = 0;
 	if (needle[0] == '\0')
-		return((char*)haystack);
-	while (*haystack != *needle)
+		return ((char*)haystack);
+	while (*haystack != *needle && *haystack != '\0')
 		haystack++;
-	while (needle[k]!='\0')
+	while (needle[k] != '\0')
 	{
 		if (haystack[k] == needle[k])
 			k++;
 		else
 		{
 			bfound = 1;
-			break;
+			break ;
 		}
 	}
 	if (bfound == 0)
-	return((char*)haystack);
+		return ((char*)haystack);
 	else
-	return (NULL);	
-}
-int	main()
-{
-	char s1[5] = "room";
-	char s2[10] = "Bedroom";
-	printf("System : %s\n", strstr(s1, s2));
-	printf("ft_strstr: %s\n" , ft_strstr(s1,s2));
-	return (0);
+		return (NULL);
 }
