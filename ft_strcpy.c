641fa95db011b1789b94eac97ffb42c9b62abd51
Nkosinathi MNCUBE <nmncube@c4r1s4.wethinkcode.co.za>
2019-05-30 11:27:53 +0200

    working

---
+++ ft_strcpy.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/30 10:57:25 by nmncube           #+#    #+#             */
+/*   Updated: 2019/05/30 11:26:59 by nmncube          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+char *ft_strcpy( char *dst , const char *scr)
+{
+	int k;
+	int j;
+	k = 0;
+	j = 0;
+
+	while (*dst)
+
+	while (s)
+	dst = (char*)scr;
+	return (dst);	
+}
+int	main()
+{
+	char *s1;
+	char s2[1];
+	s1 = "Nathi";
+	printf("System : %s\n", strcpy(s2,s1));
+	//printf("ft_strcpy: %s\n" , ft_strcpy(s2,s1));
+	return (0);
+}
+
