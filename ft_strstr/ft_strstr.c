830fe08dd0d3835bae1933852979100492e52520
Nkosinathi MNCUBE <nmncube@c4r5s8.wethinkcode.co.za>
2019-05-30 15:35:37 +0200

    Added ft_strstr.c

--- ft_strstr/ft_strstr.c
+++ ft_strstr/ft_strstr.c
@@ -6,64 +6,44 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 14:18:14 by nmncube           #+#    #+#             */
-/*   Updated: 2019/05/22 13:53:42 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/05/30 15:34:57 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
-static	char *ft_output(const char *haystack, const char *needle)
+char *ft_strstr(const char *haystack, const char *needle)
 {
+	int bfound;
 	int k;
-	const char *blank;
-	int bFound;
-	k = 0;
-	bFound = 0;
-	blank = "";
 
-	while ( needle[k] != '\0')
+	bfound = 0;
+	k = 0;
+	if (needle[0] == '\0')
+		return((char*)haystack);
+	while (*haystack != *needle)
+		haystack++;
+	while (needle[k]!='\0')
 	{
-		if ( needle [k] == haystack[k])
-			bFound = 1;
+		if (haystack[k] == needle[k])
+			k++;
 		else
 		{
-			bFound = 0;
+			bfound = 1;
 			break;
 		}
-		k++;
 	}
-	if (needle == blank)
-	return ((char *) haystack);
+	if (bfound == 0)
+	return((char*)haystack);
 	else
-	if (bFound == 0)
-	return (NULL);
-	else
-	return ((char *) haystack);	
-}
-char	*ft_strstr(const char *haystack, const char *needle)
-{
-	int k;
-	int bFound;
-	k = 0;
-	bFound = 0;
-	while ( *haystack != '\0' )
-	{
-		if ( *haystack != *needle )
-			haystack++;
-			else
-			break ;
-	}
-	return (ft_output(haystack ,needle));
+	return (NULL);	
 }
 int	main()
 {
-	char *s1;
-	char *s2;
-
-	s1 = "Bed roomi";
-	s2 = "";
-	printf("ft_stsrt : %s\n" ,ft_strstr (s1 , s2));
-	printf("System: %s\n" , strstr (s1, s2));
+	char s1[5] = "room";
+	char s2[10] = "Bedroom";
+	printf("System : %s\n", strstr(s1, s2));
+	printf("ft_strstr: %s\n" , ft_strstr(s1,s2));
 	return (0);
 }
