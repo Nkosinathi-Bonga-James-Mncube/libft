e8e45637cf96f457f34d71c6d854704852bdd95e
Nkosinathi MNCUBE <nmncube@c4r5s8.wethinkcode.co.za>
2019-05-30 14:17:36 +0200

    Added ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,39 +6,24 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 09:06:16 by nmncube           #+#    #+#             */
-/*   Updated: 2019/05/30 11:29:54 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/05/30 14:17:02 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <strings.h>
-#include <stdlib.h>
-//#include "libft.h"
+#include "libft.h"
 
-/*void ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-	
-}*/
+	char *s1;
 
-int	main()
-{
-	int s1[5];
-	//int *s1 = (int*)malloc(5 * sizeof(int));
-	size_t n;
-	int j;
-	n = 5;
-	j = 0;
-
-	bzero(s1, n);
-
-	while (j < 5)
+	s1 = (char*)s;
+	if (n == 0)
+		return (*s);
+	while (n > 0)
 	{
-		/*s1[j] = 1;
-		printf("s[%d] : %d\n", j, s1[j]);
-		j++;*/
-		printf("s[%d] : %d\n", j, s1[j]);
-		j++;
+		*s1 = '\0';
+		s1++;
+		n--;
 	}
-
-	return (0);
+	return (*s);
 }
