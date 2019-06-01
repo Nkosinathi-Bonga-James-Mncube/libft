e50136f96ce84634e9055624c9190271f1d58eaa
Nkosinathi MNCUBE <nmncube@c4r5s8.wethinkcode.co.za>
2019-06-01 14:22:29 +0200

    Added ft_strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,12 +6,12 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 10:57:25 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/01 11:24:08 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/01 14:21:35 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+#include "libft.h"
+
 char *ft_strcpy( char *dst , const char *scr)
 {
 	int k;
