e8d5535e3bf6bf506fc2cd5a899cc43ddb54c526
Nkosinathi MNCUBE <nmncube@c4r5s8.wethinkcode.co.za>
2019-06-01 14:11:16 +0200

    Added ft_strncpy.c

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,25 +6,26 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 09:31:55 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/01 11:10:39 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/01 14:10:15 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-char *ft_strncpy(char *dst, const char *src , size_t len)
+#include "libft.h"
+
+char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
-	char *s1;
-	size_t j;
-	int k;
+	char	*s1;
+	size_t	j;
+	int		k;
+
 	k = 0;
 	j = 0;
 	s1 = (char*)src;
-	while ( s1[k] != '\0' && j < len )
+	while (s1[k] != '\0' && j < len)
 	{
-				dst[k] = s1[k];
-				k++;
-				j++;
+		dst[k] = s1[k];
+		k++;
+		j++;
 	}
-	return(dst);
+	return (dst);
 }
