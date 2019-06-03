170e5bc641192e6dae4a90f11f8d6c16aa4fa95c
Nkosinathi MNCUBE <nmncube@c4r5s8.wethinkcode.co.za>
2019-06-03 12:49:06 +0200

    Added ft_isprint.c

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,15 +6,15 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:53:53 by nmncube           #+#    #+#             */
-/*   Updated: 2019/05/27 09:56:33 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/03 12:48:04 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_isprint(int c)
+int	ft_isprint(int c)
 {
-	if (c >= 32 && c <= 127)
+	if (c >= 32 && c < 127)
 		return (c = 1);
 	else
 		return (c = 0);
