66cd0273b8a36d4123d10b3f53b34cbb1bdff012
Nkosinathi-Bonga-James-Mncube <nmncube@student.wethinkcode.co.za>
2019-06-03 20:19:21 +0200

    fixed ft_atoi.h

--- ft_atoi.c
+++ ft_atoi.c
@@ -11,7 +11,8 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
+#include <string.h>
 int		ft_atoi(const char *str)
 {
 	int bfound;
@@ -19,16 +20,21 @@ int		ft_atoi(const char *str)
 	int k;
 
 	value = 0;
-	bfound = 1;
+	bfound = 0;
 	k = 0;
 	while ((*str >= 8 && *str <= 13) || (*str == 32))
 		str++;
 	if (str[0] == '-')
-		bfound = -1;
+	{
+		bfound = 1;
+		str++;
+	}
 	while ((str[k] >= 48 && str[k] <= 57) && (str[k] != '\0'))
 	{
 		value = (value * 10) + str[k] - 48;
 		k++;
 	}
+	if (bfound == 1)
+		value = value *-1;
 	return (value);
 }
