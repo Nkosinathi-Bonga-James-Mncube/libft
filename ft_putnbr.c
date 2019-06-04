59c33bb9783ab2697171c8c0b2f6ac4d373e8ddd
Nkosinathi-Bonga-James-Mncube <nmncube@student.wethinkcode.co.za>
2019-06-04 23:31:08 +0200

    Added ft_putnbr.c

---
+++ ft_putnbr.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+void	ft_putnbr(int n)
+{
+	int i;
+	char x;
+	long total;
+	i = 1000000000;
+	total = 0;
+	x = ' ';
+	if (n == 0)
+		write(1, "0",1);
+	if (n < 0)
+	{
+		write(1,"-",1);
+		n = n  *-1;
+	}
+	 while (i > n)
+		i = i /10;
+	 while (i > 0)
+	 {
+		 total = n / i;
+		 x = total + 48;
+		 write(1, &x, 1);
+		 n = n - (total * i);
+		 i = i /10;
+	 }
+}
