3fc5218ffb7b5b3c2eadc284f340e7bf04ab3df2
Event WeThinkCode <event@c4r5s6.wethinkcode.co.za>
2019-06-04 11:43:58 +0200

    Added ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,16 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/20 10:48:35 by nmncube           #+#    #+#             */
-/*   Updated: 2019/05/20 12:48:48 by nmncube          ###   ########.fr       */
+/*   Created: 2019/06/04 09:40:38 by event             #+#    #+#             */
+/*   Updated: 2019/06/04 11:43:29 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 #include <string.h>
+
+int		ft_bfound(char *c)
+{
+	int bfound;
+
+	bfound = 0;
+	if (c[0] == '-')
+		bfound = 1;
+	if (c[0] == '+')
+		bfound = 0;
+	return (bfound);
+}
+
 int		ft_atoi(const char *str)
 {
 	int bfound;
@@ -23,18 +36,20 @@ int		ft_atoi(const char *str)
 	bfound = 0;
 	k = 0;
 	while ((*str >= 8 && *str <= 13) || (*str == 32))
-		str++;
-	if (str[0] == '-')
 	{
-		bfound = 1;
 		str++;
 	}
-	while ((str[k] >= 48 && str[k] <= 57) && (str[k] != '\0'))
+	if (str[0] == '-' || str[0] == '+')
+	{
+		bfound = ft_bfound((char*)str);
+		str++;
+	}
+	while (str[k] >= 48 && str[k] <= 57 && str[k] != '+' || str[k] != '-' && str[k] != '\0')
 	{
 		value = (value * 10) + str[k] - 48;
 		k++;
 	}
 	if (bfound == 1)
-		value = value *-1;
+		value = value * -1;
 	return (value);
 }
