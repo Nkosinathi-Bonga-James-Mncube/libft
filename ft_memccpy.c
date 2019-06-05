8d0693d50a57f3fd3e942c94bd76c804587775b5
Nkosinathi MNCUBE <nmncube@c4r11s9.wethinkcode.co.za>
2019-06-05 16:24:32 +0200

    Changed ft_memccpy.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,27 +6,33 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 10:03:30 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/01 13:36:51 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/05 16:21:39 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
+#include <stdio.h>
 
 void	*ft_memccpy(void *dst, const void *scr, int c, size_t n)
 {
-	size_t			a;
+	size_t			k;
 	unsigned char	*s1;
 	unsigned char	*s2;
 
-	a = 0;
+	k = 0;
 	s1 = (unsigned char*)dst;
 	s2 = (unsigned char*)scr;
-	while (a < n)
+	while (k < n)
 	{
-		s1[a] = s2[a];
-		if (s1[a] == (unsigned char)c && n < 0)
-			return (unsigned char*)dst + a + 1;
-		a++;
+		*s1 = *s2;
+		s1++;
+		k++;
+		if (*s2 == c)
+		{
+			return ((unsigned char*)s1 + k + 1);
+		}
+		s2++;
 	}
 	return (NULL);
 }
