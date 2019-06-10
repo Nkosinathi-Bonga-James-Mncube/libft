bcda1e5be6626521dc738a9e8bd80d523929303b
Nkosinathi MNCUBE <nmncube@c4r11s9.wethinkcode.co.za>
2019-06-10 15:41:08 +0200

    Added ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	int bfound;
@@ -21,17 +21,31 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	k = 0;
 	if (needle[0] == '\0')
 		return ((char*)haystack);
-	while (*haystack != *needle && len >= ft_strlen(haystack) - 1)
-		haystack++;
+	if (len == 0)
+		return (NULL);
+	while (*haystack != *needle && len >= 0)
+		{
+			haystack++;
+			len--;
+		}
+
+	//printf("Value : %s\n" , haystack);
 	while (needle[k] != '\0')
 	{
-		if (needle[k] == haystack[k])
+		
+		if (needle[k] != haystack[k])
+			{
+				bfound = 1;
+				return (NULL);
+				//bfound = 1;
+			}
+		/*if (needle[k] == haystack[k])
 			needle++;
 		else
 		{
 			bfound = 1;
 			break ;
-		}
+		}*/
 		k++;
 	}
 	if (bfound == 0)
@@ -39,3 +53,10 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	else
 		return (NULL);
 }
+
+/*int	main()
+{
+	printf("System : %s\n", strnstr("lorem ipsum dolor sit amet", "dolar", 15));
+	printf("ft_strnstr: %s\n", ft_strnstr("lorem ipsum dolor sit lorem ipsum dolor" , "dolar",15));
+	return (0);
+}*/
