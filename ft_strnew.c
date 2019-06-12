b05a16f751fb3e0219d893e271355615e2f94523
Nkosinathi MNCUBE <nmncube@c4r12s11.wethinkcode.co.za>
2019-06-12 11:04:17 +0200

    Norminette ft_strnew.c

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,16 +6,16 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 10:18:47 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/12 10:46:36 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/12 11:03:51 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char * ft_strnew(size_t size)
+char	*ft_strnew(size_t size)
 {
 	char *s1;
-	
+
 	s1 = (char*)malloc(size + 1 * sizeof(char));
 	if (s1 != NULL && size >= 0)
 	{
@@ -24,5 +24,5 @@ char * ft_strnew(size_t size)
 		return ((char*)s1);
 	}
 	else
-		return(NULL);
+		return (NULL);
 }
