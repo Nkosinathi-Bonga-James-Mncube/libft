2f231486caa35d4b734fc886ecbccb5cc299530e
Nkosinathi MNCUBE <nmncube@c4r12s11.wethinkcode.co.za>
2019-06-12 11:06:44 +0200

    Modified libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 11:44:56 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/12 10:22:14 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/12 10:59:16 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,4 +53,5 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 char	*ft_strnew(size_t size);
 void	ft_strdel(char **as);
+void	ft_strclr(char *s);
 #endif
