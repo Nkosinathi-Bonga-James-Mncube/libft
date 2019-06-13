b87393cfbe24eb3f6d5c487a48844a9f62bba415
Nkosinathi MNCUBE <nmncube@c4r11s7.wethinkcode.co.za>
2019-06-13 12:20:57 +0200

    Added ft_strsub.c

---
+++ ft_strsub.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/13 11:39:14 by nmncube           #+#    #+#             */
+/*   Updated: 2019/06/13 12:19:01 by nmncube          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	int	k;
+	char *m;
+	k = 0;
+	m = (char*)malloc(len + 1 * sizeof(char));
+	while (len > 0)
+	{
+		m[k] = s[start];
+		len --;
+		start++;
+		k ++;
+	}
+	m[k] = '\0';
+	return (m);
+}
