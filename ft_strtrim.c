f81c7b49163bbc065daa2af2ea67ab6227e9bdde
Nkosinathi MNCUBE <nmncube@c4r11s7.wethinkcode.co.za>
2019-06-13 15:36:37 +0200

    Added ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/13 14:21:46 by nmncube           #+#    #+#             */
+/*   Updated: 2019/06/13 15:34:58 by nmncube          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s)
+{
+	int		end;
+	char	*m;
+	int		j;
+
+	j = 0;
+	while (*s == ' ' || *s == '\t' || *s == '\n')
+		s++;
+	end = ft_strlen(s) - 1;
+	if (end < 0)
+		return ((char*)s);
+	while (s[end] == 32 || s[end] == '\t' || s[end] == '\n')
+		end--;
+	m = (char*)malloc(end + 1 * sizeof(char));
+	if (m == NULL)
+		return (NULL);
+	while (end >= 0 && m != NULL)
+	{
+		m[j] = s[j];
+		j++;
+		end--;
+	}
+	m[j] = '\0';
+	return (m);
+}
