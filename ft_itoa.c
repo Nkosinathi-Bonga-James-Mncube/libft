af34da1338ad57f93ca6580e6c49dd2f2c4f96e9
Nkosinathi-Bonga-James-Mncube <nmncube@student.wethinkcode.co.za>
2019-06-16 19:12:42 +0200

    Modified ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,54 +1,23 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/15 12:13:37 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/15 15:00:28 by nmncube          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include "ft_putnbr_int.c"
 char *ft_itoa(int n)
 {
 	int k;
-	long int i;
-	long int j;
+	int i;
 	char *m;
-	k = 1;
+	long int total;
+
 	i = 1000000000;
-	j = n;
-	if (j < 0)
-		k++;
-	while(j > 10)
-	{
-		j = j / 10;
-		k++;
-	}
-	m = (char*)malloc(k + 1 *sizeof(char));
-	while (i > n)
-		i = i / 10;
 	k = 0;
-	while (i > 0)
+	total = (long int)n;
+	m = ft_memalloc((size_t)ft_int_len(total));
+	if (total == 0)
+		return((char*)ft_memset(m,'0',ft_int_len(total)));
+	if (total < 0)
 	{
-		m[k] = ft_putnbr_int(n);
-		n = n - ((m[k] - 48 ) * i);
-		i = i / 10;
+		m[0] = '-';
+		total = total * -1;
 		k++;
 	}
-	return (m);
-}
-int main()
-{
-	int n;
-	n = 1234567891;
-	//n = 2147483647;//postive bound;
-	//n = -2147483648;//negitive bound;
-	printf("Value of array:%s\n " ,ft_itoa(n));
-	return (0);
+	return (ft_loop(i,m,total,k));
 }
