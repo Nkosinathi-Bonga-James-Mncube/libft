b871152c64a85337078ff1bbda3d0470f6f4b086
Nkosinathi MNCUBE <nmncube@c4r10s6.wethinkcode.co.za>
2019-06-18 13:45:53 +0200

    Added ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 13:56:05 by nmncube           #+#    #+#             */
-/*   Updated: 2019/05/28 15:38:27 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/18 13:16:01 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,16 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*st1;
 	unsigned char	*st2;
-	int				total;
 
-	total = 0;
 	st1 = (unsigned char*)s1;
 	st2 = (unsigned char*)s2;
-	while (*st1 == *st2 && n > 0)
+	while (n > 0)
 	{
+		if (*st1 != *st2)
+			return (*st1 - *st2);
 		st1++;
 		st2++;
 		n--;
 	}
-	if (st1[0] != st2[0] && n != 0)
-	{
-		total = (int)st1[0] - (int)st2[0];
-		return (total);
-	}
-	else
-		return (0);
+	return (0);
 }
