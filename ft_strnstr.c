4465356a70d4a2a1027fb6b2b9f10ccc2a2f9267
Nkosinathi MNCUBE <nmncube@c4r10s6.wethinkcode.co.za>
2019-06-18 13:46:40 +0200

    Added ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,57 +6,35 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:05:27 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/01 14:32:53 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/18 13:17:00 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int bfound;
-	int k;
+	const char	*s1;
+	const char	*s2;
+	size_t		s;
 
-	bfound = 0;
-	k = 0;
-	if (needle[0] == '\0')
+	if (*needle == '\0')
 		return ((char*)haystack);
-	if (len == 0)
-		return (NULL);
-	while (*haystack != *needle && len >= 0)
-		{
-			haystack++;
-			len--;
-		}
-
-	//printf("Value : %s\n" , haystack);
-	while (needle[k] != '\0')
+	while (*haystack != '\0' && len > 0)
 	{
-		
-		if (needle[k] != haystack[k])
-			{
-				bfound = 1;
-				return (NULL);
-				//bfound = 1;
-			}
-		/*if (needle[k] == haystack[k])
-			needle++;
-		else
+		s1 = haystack;
+		s2 = needle;
+		s = len;
+		while (*s2 != '\0' && *s1 == *s2 && s > 0)
 		{
-			bfound = 1;
-			break ;
-		}*/
-		k++;
+			s1++;
+			s2++;
+			s--;
+		}
+		if (*s2 == '\0')
+			return ((char*)haystack);
+		haystack++;
+		len--;
 	}
-	if (bfound == 0)
-		return ((char*)haystack);
-	else
-		return (NULL);
-}
-
-/*int	main()
-{
-	printf("System : %s\n", strnstr("lorem ipsum dolor sit amet", "dolar", 15));
-	printf("ft_strnstr: %s\n", ft_strnstr("lorem ipsum dolor sit lorem ipsum dolor" , "dolar",15));
 	return (0);
-}*/
+}
