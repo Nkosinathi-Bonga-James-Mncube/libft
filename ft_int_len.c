8a2f6bd26461b3c41c1204d997e923b85e43efbe
Nkosinathi MNCUBE <nmncube@c4r10s8.wethinkcode.co.za>
2019-06-19 14:28:04 +0200

    Modified ft_int_len.c

--- ft_int_len.c
+++ ft_int_len.c
@@ -1,17 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_int_len.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/19 14:21:59 by nmncube           #+#    #+#             */
+/*   Updated: 2019/06/19 14:24:41 by nmncube          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_int_len(int n)
-{	
-	int k;
-	long int total;
-	
+#include "libft.h"
+
+int	ft_int_len(int n)
+{
+	int			k;
+	long int	total;
+
 	k = 1;
 	total = (long int)n;
 	if (total == 0)
 		return (k);
 	if (total < 0)
-	{ 
+	{
 		k++;
 		total = total * -1;
 	}
