e83157c2a8cc4eb1a0c895af9097709b94273a54
Nkosinathi MNCUBE <nmncube@c4r10s8.wethinkcode.co.za>
2019-06-19 14:55:56 +0200

    Norminette ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/19 14:51:46 by nmncube           #+#    #+#             */
+/*   Updated: 2019/06/19 14:55:21 by nmncube          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
-char *ft_itoa(int n)
+
+char	*ft_itoa(int n)
 {
-	int k;
-	int i;
-	char *m;
-	long int total;
+	int			k;
+	int			i;
+	long int	total;
+	char		*m;
 
 	i = 1000000000;
 	k = 0;
@@ -14,12 +26,12 @@ char *ft_itoa(int n)
 	if (m == NULL)
 		return (NULL);
 	if (total == 0)
-		return((char*)ft_memset(m,'0',ft_int_len(total)));
+		return ((char*)ft_memset(m, '0', ft_int_len(total)));
 	if (total < 0)
 	{
 		m[0] = '-';
 		total = total * -1;
 		k++;
 	}
-	return (ft_loop(i,m,total,k));
+	return (ft_loop(i, m, total, k));
 }
