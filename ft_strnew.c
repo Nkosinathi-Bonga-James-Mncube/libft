b42f3e8cf283c537cf6761c659a62f08a9e3e9bb
Nkosinathi MNCUBE <nmncube@c4r11s6.wethinkcode.co.za>
2019-06-23 10:51:19 +0200

    Correct ft_strnew.c

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 10:18:47 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/12 11:03:51 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/23 10:45:55 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strnew(size_t size)
 	s1 = (char*)malloc(size + 1 * sizeof(char));
 	if (s1 != NULL)
 	{
-		size--;
+		size++;
 		ft_bzero(s1, size);
 		return ((char*)s1);
 	}
