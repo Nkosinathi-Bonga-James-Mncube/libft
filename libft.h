eddde5086823d422e3b0f1e1fb0ba517b639b0bb
Nkosinathi MNCUBE <nmncube@c4r10s6.wethinkcode.co.za>
2019-06-26 12:39:56 +0200

    Modified libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 11:44:56 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/26 12:27:28 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/26 12:39:34 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
-void	*ft_memccpy(void *dst, const void *scr, int c, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
@@ -35,7 +35,7 @@ void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strchr(const char *s, int c);
 int		ft_strcmp(const char *s1, const char *s2);
-char	*ft_strcpy(char *dst, const char *scr);
+char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *s1);
 size_t	ft_strlcat(char *dst, const char *src, size_t l);
 size_t	ft_strlen(const char *s);
