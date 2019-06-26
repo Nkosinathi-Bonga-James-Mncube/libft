8e357791fdb071c4e1443ec18337b10487f3d4f5
Nkosinathi MNCUBE <nmncube@c4r10s6.wethinkcode.co.za>
2019-06-26 12:29:43 +0200

    Modified libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 11:44:56 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/21 10:19:48 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/26 12:27:28 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ int		ft_isprint(int c);
 void	*ft_memccpy(void *dst, const void *scr, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memcpy(void *dst, const void *scr, size_t n);
-void	*ft_memmove(void *dst, const void *scr, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strchr(const char *s, int c);
