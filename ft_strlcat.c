8444c30f54e98d2625d0375e243fea8eca29ba86
Nkosinathi MNCUBE <nmncube@c4r10s6.wethinkcode.co.za>
2019-06-26 12:46:24 +0200

    Modified ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,31 +6,31 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 09:47:32 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/23 11:00:08 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/26 12:45:28 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t l)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t k;
 	size_t m;
 
-	if (ft_strlen(dst) >= l)
-		return (l + ft_strlen(src));
-	if (l == 0)
+	if (ft_strlen(dst) >= dstsize)
+		return (dstsize + ft_strlen(src));
+	if (dstsize == 0)
 		return (ft_strlen(src));
 	k = ft_strlen(src);
 	m = 0;
-	while (*dst && l > 0)
+	while (*dst && dstsize > 0)
 	{
 		dst++;
-		l--;
+		dstsize--;
 		m++;
 	}
-	ft_strncat(dst, (char*)src, l - 1);
-	if (l == 1 || *src == 0)
+	ft_strncat(dst, (char*)src, dstsize - 1);
+	if (dstsize == 1 || *src == 0)
 		*dst = '\0';
 	return (k + m);
 }
