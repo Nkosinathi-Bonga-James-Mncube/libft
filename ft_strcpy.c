225964f7f8ef184c870e22b2b52360dcb7286cd0
Nkosinathi MNCUBE <nmncube@c4r10s6.wethinkcode.co.za>
2019-06-26 12:35:05 +0200

    Modified ft_strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,20 +6,20 @@
 /*   By: nmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 14:24:15 by nmncube           #+#    #+#             */
-/*   Updated: 2019/06/07 14:43:20 by nmncube          ###   ########.fr       */
+/*   Updated: 2019/06/26 12:34:37 by nmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcpy(char *dst, const char *scr)
+char	*ft_strcpy(char *dst, const char *src)
 {
 	int		k;
 
 	k = 0;
-	while (scr[k] != '\0')
+	while (src[k] != '\0')
 	{
-		dst[k] = scr[k];
+		dst[k] = src[k];
 		k++;
 	}
 	dst[k] = '\0';
